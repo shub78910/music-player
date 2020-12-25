238f613d2491ac9e77a0be10639105c8f360cf8f
shub78910 <64699670+shub78910@users.noreply.github.com>
2020-12-25 12:57:46 +0530

    first submit
    done, the bare minimun, play, pause.
    will be adding more functions.

---
+++ index.js
@@ -0,0 +1,10 @@
+// const img = document.getElementById("image");
+// const music = document.getElementById("audio");
+// const play = document.querySelector(".play");
+
+
+//     play.addEventListener("click",()=>{
+//     music.play();
+//     play.classList.replace("fa-play","fa-pause");
+//     img.classList.add("animation");
+// })
